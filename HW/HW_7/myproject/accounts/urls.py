e521a648ac3f450c115d515288b1526069eecbeb
Noewewwwew <dnjs4475@naver.com>
2025-05-13 23:27:46 +0900

    Update

--- HW/HW_7/myproject/accounts/urls.py
+++ HW/HW_7/myproject/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),    # (1점)
-    path('logout/', views.logout_view, name='logout'),  # (1점)
-    path('home/', views.home_view, name='home'),        # (1점)
+    path('login/', views.login_view, name='login'),    
+    path('logout/', views.logout_view, name='logout'),  
+    path('home/', views.home_view, name='home'),        
 ]
